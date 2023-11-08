d07371b69ac63dcbb1e409aeda0e9c30c4a399d2
H.Karimi <hrk13811396@gmail.com>
2023-11-08 13:32:04 +0330

    implement listener for notification module & test it

--- notification/src/main/java/com/hrk/notification/NotificationApplication.java
+++ notification/src/main/java/com/hrk/notification/NotificationApplication.java
@@ -2,12 +2,31 @@ package com.hrk.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.hrk.notification",
+                "com.hrk.amqp"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig
+//    ) {
+//        return args -> {
+//            producer.publish(
+//                    new Person("omid", 24),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey()
+//            );
+//        };
+//    }
+//
+//    record Person(String name, int age){};
 }
