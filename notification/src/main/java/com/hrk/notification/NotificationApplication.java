49153904909de6ca86f3b5059281c489a30eeeb9
H.Karimi <hrk13811396@gmail.com>
2023-11-23 00:06:34 +0330

    refactor microservices for run with kubernetes

--- notification/src/main/java/com/hrk/notification/NotificationApplication.java
+++ notification/src/main/java/com/hrk/notification/NotificationApplication.java
@@ -2,6 +2,8 @@ package com.hrk.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -9,6 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 "com.hrk.amqp"
         }
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
